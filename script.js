6c16bf54e81006c5072f758c3162fc0def1b4395
mcduffchannel <mcduffchannel@gmail.com>
2023-12-05 16:54:12 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-function fetchTemplate(loadType) {
+function fetchTemplate(loadType, rotPos) {
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             // Once the text is fetched, call updateText with the template
-            updateText(this.responseText.trim());
+            updateText(this.responseText.trim(), rotPos);
         }
     };
     xhr.open("GET", loadType, true);
     xhr.send();
 }
 
-function updateText(template) {
+function updateText(template, rotPos) {
     var fieldLocationVal = document.getElementById("fieldLocation").value;
     var fieldFNameVal = document.getElementById("fieldFName").value;
     var fieldLNameVal = document.getElementById("fieldLName").value;
@@ -21,6 +21,7 @@ function updateText(template) {
     result = result.replace('${txtFName}', fieldFNameVal);
     result = result.replace('${txtLName}', fieldLNameVal);
     result = result.replace('${txtDate}', fieldDateVal);
+    result = result.replace('${txtPos}', rotPos);
     
     var parser = new DOMParser();
     var doc = parser.parseFromString(result, "image/svg+xml");
@@ -53,19 +54,20 @@ function downloadSVG() {
 }
 
 var tetrisType = "assets/Tetris-J.svg";
+var rotPos = "0.7071 -0.7071 0.7071 0.7071"
 
 document.getElementById("downloadBtn").addEventListener("click", downloadSVG);
-document.getElementById("oBtn").addEventListener("click", function() { tetrisType = "assets/Tetris-O.svg"; fetchTemplate(tetrisType); });
-document.getElementById("iBtn").addEventListener("click", function() { tetrisType = "assets/test.txt"; fetchTemplate(tetrisType); });
-document.getElementById("sBtn").addEventListener("click", function() { tetrisType = "assets/test.txt"; fetchTemplate(tetrisType); });
-document.getElementById("zBtn").addEventListener("click", function() { tetrisType = "assets/test.txt"; fetchTemplate(tetrisType); });
-document.getElementById("lBtn").addEventListener("click", function() { tetrisType = "assets/test.txt"; fetchTemplate(tetrisType); });
-document.getElementById("jBtn").addEventListener("click", function() { tetrisType = "assets/Tetris-J.svg"; fetchTemplate(tetrisType); });
-document.getElementById("tBtn").addEventListener("click", function() { tetrisType = "assets/test.txt"; fetchTemplate(tetrisType); });
+document.getElementById("oBtn").addEventListener("click", function() { tetrisType = "assets/Tetris-O.svg"; fetchTemplate(tetrisType, rotPos); });
+document.getElementById("iBtn").addEventListener("click", function() { tetrisType = "assets/test.txt"; fetchTemplate(tetrisType, rotPos); });
+document.getElementById("sBtn").addEventListener("click", function() { tetrisType = "assets/test.txt"; fetchTemplate(tetrisType, rotPos); });
+document.getElementById("zBtn").addEventListener("click", function() { tetrisType = "assets/test.txt"; fetchTemplate(tetrisType, rotPos); });
+document.getElementById("lBtn").addEventListener("click", function() { tetrisType = "assets/test.txt"; fetchTemplate(tetrisType, rotPos); });
+document.getElementById("jBtn").addEventListener("click", function() { tetrisType = "assets/Tetris-J.svg"; fetchTemplate(tetrisType, rotPos); });
+document.getElementById("tBtn").addEventListener("click", function() { tetrisType = "assets/test.txt"; fetchTemplate(tetrisType, rotPos); });
 
-document.getElementById("fieldLocation").addEventListener("input", () => fetchTemplate(tetrisType));
-document.getElementById("fieldFName").addEventListener("input", () => fetchTemplate(tetrisType));
-document.getElementById("fieldLName").addEventListener("input", () => fetchTemplate(tetrisType));
-document.getElementById("fieldDate").addEventListener("input", () => fetchTemplate(tetrisType));
+document.getElementById("fieldLocation").addEventListener("input", () => fetchTemplate(tetrisType, rotPos));
+document.getElementById("fieldFName").addEventListener("input", () => fetchTemplate(tetrisType, rotPos));
+document.getElementById("fieldLName").addEventListener("input", () => fetchTemplate(tetrisType, rotPos));
+document.getElementById("fieldDate").addEventListener("input", () => fetchTemplate(tetrisType, rotPos));
 
-fetchTemplate(tetrisType);
+fetchTemplate(tetrisType, rotPos);
